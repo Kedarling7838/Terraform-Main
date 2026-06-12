7b26364753f8b57571c6e4ad9d2cd10f78bd1905
kd <kedarlingkanade07@gmail.com>
2026-06-12 19:53:53 +0530

    commit

---
+++ Day-14-locals/main.tf
@@ -0,0 +1,16 @@
+locals {
+  bucket-name = "${var.layer}-${var.env}-bucket-hydnaresh"
+}
+
+resource "aws_s3_bucket" "demo" {
+  # bucket = "web-test-bucket-hydnaresh"
+  # bucket = "${var.layer}-${var.env}-bucket-hyd"
+
+  bucket = local.bucket-name
+
+  tags = {
+    # Name = "${var.layer}-${var.env}-bucket-hyd"
+    Name        = local.bucket-name
+    Environment = var.env
+  }
+}
---
+++ Day-14-locals/variables.tf
@@ -0,0 +1,9 @@
+variable "layer" {
+  type    = string
+  default = "web"
+}
+
+variable "env" {
+  type    = string
+  default = "test"
+}
